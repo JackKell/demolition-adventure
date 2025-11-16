e49fb5b943e3aa7bcf42ac97a7c39f1afb3ef46a
Brandon Olson <olson.l.brandon@gmail.com>
2025-11-16 11:47:59 -0800

    refactor: fallaway tile
    Don't destroy the fallaway tile when falling. This will make it easier to undo and redo later

--- levels/common/fallaway_tile.gd
+++ levels/common/fallaway_tile.gd
@@ -7,10 +7,17 @@ const FALL_ANIMATION_DURATION: float = 0.6
 
 func handle_entity_exit(exiting_entity: Entity) -> void:
 	if exiting_entity is Character:
-		level._coords_to_tile.erase(coords)
-		var t = create_tween()
-		t.tween_property(mesh, "position", Vector3.DOWN, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN).as_relative()
-		t.parallel().tween_property(mesh, "rotation_degrees:x", 45, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN)
-		t.parallel().tween_property(mesh, "rotation_degrees:z", 45, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN)
-		await t.finished
-		queue_free()
+		fall()
+
+func fall() -> void:
+	type = TileType.OBSTICAL
+	var t = create_tween()
+	t.tween_property(mesh, "position", Vector3.DOWN, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN).as_relative()
+	t.parallel().tween_property(mesh, "rotation_degrees:x", 45, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN)
+	t.parallel().tween_property(mesh, "rotation_degrees:z", 45, FALL_ANIMATION_DURATION).set_ease(Tween.EASE_IN)
+	t.tween_property(mesh, "visible", false, 0)
+	
+func raise() -> void:
+	type = TileType.NORMAL
+	mesh.position = Vector3.ZERO
+	mesh.visible = true
